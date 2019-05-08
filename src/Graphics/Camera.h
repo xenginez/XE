bf48cecd1ec1da554a7c6036f9270ada368f9e4d
ZhengYuanQing <zhengyuanqing.95@gmail.com>
2019-05-09 07:38:00 +0800

    增加Camera和ParticleSystem类

---
+++ src/Graphics/Camera.h
@@ -0,0 +1,20 @@
+/*!
+ * \file    Camera.h
+ *
+ * \author  ZhengYuanQing
+ * \date    2019-05-09
+ * \email   zhengyuanqing.95@gmail.com
+ *
+ */
+#ifndef __CAMERA_H__D382481F_53EE_43BE_A079_66487F3F4936
+#define __CAMERA_H__D382481F_53EE_43BE_A079_66487F3F4936
+
+class Camera
+{
+public:
+	Camera();
+	
+	~Camera();
+};
+
+#endif //__CAMERA_H__D382481F_53EE_43BE_A079_66487F3F4936
---
+++ src/Graphics/ParticleSystem.h
@@ -0,0 +1,20 @@
+/*!
+ * \file    ParticleSystem.h
+ *
+ * \author  ZhengYuanQing
+ * \date    2019-05-09
+ * \email   zhengyuanqing.95@gmail.com
+ *
+ */
+#ifndef __PARTICLESYSTEM_H__43C62B4D_0713_4D3C_9911_644F6050A461
+#define __PARTICLESYSTEM_H__43C62B4D_0713_4D3C_9911_644F6050A461
+
+class ParticleSystem
+{
+public:
+	ParticleSystem();
+	
+	~ParticleSystem();
+};
+
+#endif //__PARTICLESYSTEM_H__43C62B4D_0713_4D3C_9911_644F6050A461
