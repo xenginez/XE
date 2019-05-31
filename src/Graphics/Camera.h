d6171e5acb5f8e4fc49757f377255b162a924312
ZhengYuanQing <zhengyuanqing.95@gmail.com>
2019-05-31 23:09:40 +0800

    Graphics添加几个类型

---
+++ src/Graphics/Camera.h
@@ -0,0 +1,20 @@
+/*!
+ * \file    Camera.h
+ *
+ * \author  ZhengYuanQing
+ * \date    2019-05-31
+ * \email   zhengyuanqing.95@gmail.com
+ *
+ */
+#ifndef __XE_CAMERA_H__
+#define __XE_CAMERA_H__
+
+class Camera
+{
+public:
+	Camera();
+	
+	~Camera();
+};
+
+#endif //__XE_CAMERA_H__
