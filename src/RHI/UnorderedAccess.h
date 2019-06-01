c64ed70879b79bf62cb1ee896171e08e48a4f631
ZhengYuanQing <zhengyuanqing.95@gmail.com>
2019-06-01 08:35:35 +0800

    Graphics添加几个类型

---
+++ src/RHI/UnorderedAccess.h
@@ -0,0 +1,20 @@
+/*!
+ * \file    UnorderedAccess.h
+ *
+ * \author  ZhengYuanQing
+ * \date    2019-06-01
+ * \email   zhengyuanqing.95@gmail.com
+ *
+ */
+#ifndef __XE_UNORDEREDACCESS_H__
+#define __XE_UNORDEREDACCESS_H__
+
+class UnorderedAccess
+{
+public:
+	UnorderedAccess();
+	
+	~UnorderedAccess();
+};
+
+#endif //__XE_UNORDEREDACCESS_H__
