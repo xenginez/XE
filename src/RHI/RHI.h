e1f610632170ce70a518dd375be428383b893fd9
ZhengYuanQing <zhengyuanqing.95@gmail.com>
2019-06-05 21:38:14 +0800

    Camera从Grapics移动到RHI中

--- src/RHI/RHI.h
+++ src/RHI/RHI.h
@@ -9,6 +9,49 @@
 #ifndef __RHI_H__B57E1D98_CE7B_4EF9_BEEC_C2CB1911EEB2
 #define __RHI_H__B57E1D98_CE7B_4EF9_BEEC_C2CB1911EEB2
 
+#include "Resource.h"
+
+#include "GFX.h"
 #include "Image.h"
+#include "Fence.h"
+#include "Camera.h"
+#include "Sampler.h"
+#include "Uniform.h"
+#include "Viewport.h"
+#include "InputLayout.h"
+#include "RenderTarget.h"
+#include "OcclusionQuery.h"
+
+#include "BlendState.h"
+#include "RasterizerState.h"
+#include "DepthStencilState.h"
+#include "ComputePipelineState.h"
+#include "GraphicsPipelineState.h"
+
+#include "Buffer.h"
+#include "FrameBuffer.h"
+#include "IndexBuffer.h"
+#include "PixelShader.h"
+#include "VertexBuffer.h"
+#include "ComputeBuffer.h"
+#include "UniformBuffer.h"
+#include "ConstantBuffer.h"
+#include "DepthStencilBuffer.h"
+#include "DynamicIndexBuffer.h"
+#include "DynamicVertexBuffer.h"
+
+#include "Shader.h"
+#include "HullShader.h"
+#include "VertexShader.h"
+#include "DomainShader.h"
+#include "ComputeShader.h"
+#include "GeometryShader.h"
+
+#include "Texture.h"
+#include "Texture2D.h"
+#include "Texture3D.h"
+#include "TextureCube.h"
+#include "Texture2DArray.h"
+#include "UnorderedAccess.h"
 
 #endif //__RHI_H__B57E1D98_CE7B_4EF9_BEEC_C2CB1911EEB2
