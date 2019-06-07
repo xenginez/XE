ac2458144f1a577716565f7c5096b82dc93657c6
ZhengYuanQing <zhengyuanqing.95@gmail.com>
2019-06-07 22:07:06 +0800

    StaticMesh 重命名 Mesh

---
+++ src/Graphics/Mesh.h
@@ -0,0 +1,20 @@
+/*!
+ * \file    Mesh.h
+ *
+ * \author  ZhengYuanQing
+ * \date    2019-05-31
+ * \email   zhengyuanqing.95@gmail.com
+ *
+ */
+#ifndef __XE_MESH_H__
+#define __XE_MESH_H__
+
+class Mesh
+{
+public:
+	Mesh();
+	
+	~Mesh();
+};
+
+#endif //__XE_MESH_H__
