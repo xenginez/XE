80b455c2b9d9cce4c9e53b85984b58e3c14fac2a
ZhengYuanQing <zhengyuanqing.95@gmail.com>
2019-06-09 11:23:48 +0800

    ParticleSystem重命名为Particle

---
+++ src/Graphics/Particle.h
@@ -0,0 +1,20 @@
+/*!
+ * \file    Particle.h
+ *
+ * \author  ZhengYuanQing
+ * \date    2019-06-09
+ * \email   zhengyuanqing.95@gmail.com
+ *
+ */
+#ifndef __PARTICLE_H__462C0D0F_B91A_42F1_B0E5_F9FBD9A145C1
+#define __PARTICLE_H__462C0D0F_B91A_42F1_B0E5_F9FBD9A145C1
+
+class Particle
+{
+public:
+	Particle();
+	
+	~Particle();
+};
+
+#endif //__PARTICLE_H__462C0D0F_B91A_42F1_B0E5_F9FBD9A145C1
