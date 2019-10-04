e0356b12381048de7adbeb889c0d984d2bd4f500
ZhengYuanQing <zhengyuanqing.95@gmail.com>
2019-10-04 11:08:27 +0800

    删除openal依赖

--- src/Core/AudioService.cpp
+++ src/Core/AudioService.cpp
@@ -1,8 +1,5 @@
 #include "AudioService.h"
 
-#include <AL/al.h>
-#include <AL/alc.h>
-
 USING_XE
 
 BEG_META( AudioService )
@@ -20,17 +17,7 @@ XE::AudioService::~AudioService()
 
 bool XE::AudioService::Startup()
 {
-	auto device = alcOpenDevice( nullptr );
-	auto context = alcCreateContext( device, nullptr );
-
-	alcMakeContextCurrent( context );
-
-	alDistanceModel( AL_INVERSE_DISTANCE );
-
-	alDopplerFactor( 1 );
-	alDopplerVelocity( 343 );
-
-	return true;
+	return false;
 }
 
 void XE::AudioService::Update()
@@ -40,13 +27,7 @@ void XE::AudioService::Update()
 
 void XE::AudioService::Clearup()
 {
-	auto context = alcGetCurrentContext();
-	auto device = alcGetContextsDevice( context );
 
-	alcMakeContextCurrent( 0 );
-
-	alcDestroyContext( context );
-	alcCloseDevice( device );
 }
 
 void XE::AudioService::Prepare()
