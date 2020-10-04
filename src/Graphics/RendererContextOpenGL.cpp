f4a6c79541f89dc7cd153224fbb16a5f4a540566
zyq <zhengyuanqing.95@gmail.com>
2020-10-04 15:35:24 +0800

    opengl整合为1个

--- src/Graphics/RendererContextOpenGL.cpp
+++ src/Graphics/RendererContextOpenGL.cpp
@@ -1,5 +1,13 @@
 #include "RendererContextOpenGL.h"
 
+#include "GLContext.h"
+#include "GLContextWGL.h"
+#include "GLContextEGL.h"
+#include "GLContextGLX.h"
+#include "GLContextEAGL.h"
+#include "GLContextNSGL.h"
+#include "GLContextHTML5.h"
+
 XE::RendererContextGL::RendererContextGL()
 {
 
