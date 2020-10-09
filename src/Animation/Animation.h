ff75c26ac8ef7a50e8ceb6f2d1904be74b4130a0
zyq <zhengyuanqing.95@gmail.com>
2020-10-09 21:36:33 +0800

    使用ozz作为动画模块

--- src/Animation/Animation.h
+++ src/Animation/Animation.h
@@ -9,13 +9,4 @@
 #ifndef ANIMATION_H__2DF5C375_5088_41EB_8C3F_CEA376A63895
 #define ANIMATION_H__2DF5C375_5088_41EB_8C3F_CEA376A63895
 
-#include "Animator.h"
-#include "AnimationState.h"
-#include "AnimationTrigger.h"
-#include "AnimationIKSolver.h"
-#include "SkeletonAnimation.h"
-#include "AnimationCondition.h"
-#include "AnimationTransition.h"
-#include "AnimationController.h"
-
 #endif // ANIMATION_H__2DF5C375_5088_41EB_8C3F_CEA376A63895
--- src/Animation/Type.h
+++ src/Animation/Type.h
@@ -14,38 +14,6 @@
 
 BEG_XE_NAMESPACE
 
-DECL_PTR( Animator );
-DECL_PTR( AnimationState );
-DECL_PTR( AnimationTrigger );
-DECL_PTR( SkeletonAnimation );
-DECL_PTR( AnimationIKSolver );
-DECL_PTR( AnimationCondition );
-DECL_PTR( AnimationTransition );
-DECL_PTR( AnimationController );
-
-DECL_HANDLE( XE_API, AnimationIKSolver );
-
-enum class AnimationCompare
-{
-	EQUAL,
-	LESS,
-	GREA,
-	LEQUAL,
-	GEQUAL,
-	NOTEQUAL,
-};
-DECL_META_ENUM( XE_API, AnimationCompare );
-
-
-enum class IKAlgorithm
-{
-	ONE_BONE,
-	TWO_BONE,
-	FABRIK,
-	MSS,
-};
-DECL_META_ENUM( XE_API, IKAlgorithm );
-
 END_XE_NAMESPACE
 
 #endif // __TYPE_H__E9FBF2A2_BC47_464B_AAAE_44C511E818E3
