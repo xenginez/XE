ff1f1df0be3b7b1023a44f7a819a9df6df0156a8
zyq <zhengyuanqing.95@gmail.com>
2020-10-14 22:25:06 +0800

    去除多余类型

--- src/Physics/Type.h
+++ src/Physics/Type.h
@@ -15,19 +15,14 @@
 BEG_XE_NAMESPACE
 
 DECL_PTR( Shape );
-DECL_PTR( Pruning );
 DECL_PTR( RigidBody );
-DECL_PTR( Aggregate );
 DECL_PTR( Constraint );
 DECL_PTR( Collection );
 DECL_PTR( PhysicsScene );
 DECL_PTR( PhysicsMaterial );
-DECL_PTR( ConstraintConnector );
 
 DECL_HANDLE( XE_API, Shape );
-DECL_HANDLE( XE_API, Pruning );
 DECL_HANDLE( XE_API, RigidBody );
-DECL_HANDLE( XE_API, Aggregate );
 DECL_HANDLE( XE_API, Constraint );
 DECL_HANDLE( XE_API, Collection );
 DECL_HANDLE( XE_API, PhysicsScene );
